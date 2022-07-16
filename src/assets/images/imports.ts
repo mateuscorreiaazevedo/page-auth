d2f70ee6b93c94f114df24fb6520c1382046e1de
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-07-16 02:13:16 -0300

    chore: changing favicon and adding a new image to ./assets/images folder

--- src/assets/images/imports.ts
+++ src/assets/images/imports.ts
@@ -3,13 +3,15 @@ import dogRobot from './robot-dog.png'
 import peopleOne from './people-1.png'
 import peopleTwo from './people-2.png'
 import peopleThree from './people-3.png'
+import userIcon from './user.png'
 
 const images = {
   dogRobot,
   helloRobot,
   peopleOne,
   peopleThree,
-  peopleTwo
+  peopleTwo,
+  userIcon
 }
 
 export default images
