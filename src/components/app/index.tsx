55d62047b8ab4861f5d564592bcb84e1ab918bc1
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-07-17 01:02:06 -0300

    chore: import a attribute CSS of the react-slick

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -2,8 +2,10 @@ import { ContextThemeProvider } from '../../contexts/theme-context'
 import GlobalStyle from '../../assets/styles/global.style'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from '../../routes/routes'
-import React from 'react'
+import 'slick-carousel/slick/slick-theme.css'
 import { ToggleMenu } from '../toggle-menu'
+import 'slick-carousel/slick/slick.css'
+import React from 'react'
 
 function App () {
   return (
