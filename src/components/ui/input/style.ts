b4070a0dc1541aa56c70142e78355032dec3e5bf
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-07-18 16:19:12 -0300

    style: applying padding-right on the input component so its value doesn't override the view password button

--- src/components/ui/input/style.ts
+++ src/components/ui/input/style.ts
@@ -8,7 +8,7 @@ export const Input = styled.input`
   width: 340px;
   height: 52px;
   padding: 8px 15px;
-  padding-right: 40px;
+  padding-right: 45px;
   font-size: 1rem;
   border-radius: 12px;
   border: none;
