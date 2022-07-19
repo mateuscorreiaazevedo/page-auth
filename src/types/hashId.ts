0bf08410df93b819443508f1f26e0bde989a1af2
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-07-19 01:35:35 -0300

    type: add new types of the project

---
+++ src/types/hashId.ts
@@ -0,0 +1,12 @@
+export async function hash (): Promise<string> {
+  const message = (+new Date()).toString()
+
+  const msgBuffer = new TextEncoder().encode(message)
+
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
+
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
+  return hashHex
+}
